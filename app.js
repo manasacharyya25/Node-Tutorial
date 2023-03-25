61d9ac41975fdf684d9b7556a41a7a02915e51bd
Manas Acharyya <monteprofilityprod@gmail.com>
2023-03-25 16:54:16 +0530

    Routing, Post Message, Read Message as input stream

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    url = req.url;
+    method = req.method;
+
+    if(url==='/input') {
+        html = `
+            <html>
+                <header>
+                    <title>Input</title>
+                </header>
+                <body>
+                    <form action="/submit" method="POST">       
+                        <input type="text" name="input" />
+                        <input type="submit" value="submit" />
+                    </form>
+                </body>
+            </html>
+        `
+        // form with get method sends input as request parameters
+        // form with post method sends input as form data
+        res.write(html);
+        return res.end();
+    }
+
+    if (url==='/submit') {
+        const data = []
+
+        req.on("data", (chunk) => {
+            data.push(chunk)
+        })
+
+        req.on("end", () => {
+            input = Buffer.concat(data).toString();
+            console.log(input);
+        })
+
+        res.statusCode = '302';
+        res.setHeader("Location", "/input");
+        return res.end();
+    }
+
     res.setHeader('Content-Type','text/html');
     res.write("<html><header><title>Server</title><header><body>Hello World !!!</body></html>");
     res.end();
