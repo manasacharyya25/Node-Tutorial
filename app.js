4cd7c71189adcd018da96da0832ac0e200d1fe3e
Manas Acharyya <monteprofilityprod@gmail.com>
2023-03-25 15:49:10 +0530

    Writing Response

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type','text/html');
+    res.write("<html><header><title>Server</title><header><body>Hello World !!!</body></html>");
+    res.end();
 })
 
 server.listen(3000);
